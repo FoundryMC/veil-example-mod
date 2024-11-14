3dece225bcd114965d56a2143d537b4ae75d642d
Ocelot <FinntheRaider@gmail.com>
2024-11-14 12:43:12 -0700

    Added test for cubemap and fbo wrapper

--- src/main/java/foundry/veil/example/VeilExampleModClient.java
+++ src/main/java/foundry/veil/example/VeilExampleModClient.java
@@ -1,6 +1,9 @@
 package foundry.veil.example;
 
 import foundry.veil.api.client.render.VeilRenderBridge;
+import foundry.veil.api.client.render.framebuffer.AdvancedFbo;
+import foundry.veil.api.client.render.framebuffer.AdvancedFboAttachment;
+import foundry.veil.api.client.render.texture.DynamicCubemapTexture;
 import foundry.veil.api.event.VeilRenderLevelStageEvent;
 import foundry.veil.example.blockentity.MapBlockEntity;
 import foundry.veil.example.client.render.MapBlockEntityRenderer;
@@ -28,6 +31,31 @@ public class VeilExampleModClient implements ClientModInitializer {
         FabricVeilRenderLevelStageEvent.EVENT.register((stage, levelRenderer, bufferSource, poseStack, projectionMatrix, renderTick, partialTicks, camera, frustum) -> {
             if (stage == VeilRenderLevelStageEvent.Stage.AFTER_LEVEL) {
                 MirrorBlockEntityRenderer.renderLevel(Minecraft.getInstance().level, projectionMatrix, partialTicks, VeilRenderBridge.create(frustum), camera);
+            }
+        });
+
+        FabricVeilRenderLevelStageEvent.EVENT.register((stage, levelRenderer, bufferSource, poseStack, projectionMatrix, renderTick, partialTicks, camera, frustum) -> {
+            if(stage != VeilRenderLevelStageEvent.Stage.AFTER_LEVEL) {
+                try (DynamicCubemapTexture cubemap = new DynamicCubemapTexture()) {
+                    cubemap.init(64, 64);
+                    AdvancedFbo fbo = AdvancedFbo.withSize(64, 64).addColorTextureWrapper(cubemap.getId(), 0).build(true);
+                    fbo.bind(false);
+                    fbo.setColorAttachmentTexture(0, cubemap.getId(), 1);
+                    AdvancedFbo.unbind();
+                    fbo.free();
+                }
+            }
+        });
+
+        // Make sure there's no crash
+        FabricVeilRendererEvent.EVENT.register(renderer -> {
+            try (DynamicCubemapTexture cubemap = new DynamicCubemapTexture()) {
+                cubemap.init(64, 64);
+                AdvancedFbo fbo = AdvancedFbo.withSize(64, 64).addColorTextureWrapper(cubemap.getId(), 0).build(true);
+                fbo.bind(false);
+                fbo.setColorAttachmentTexture(0, cubemap.getId(), 1);
+                AdvancedFbo.unbind();
+                fbo.free();
             }
         });
     }
